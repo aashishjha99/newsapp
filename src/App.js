77999ea564d4479b9aca93d6b35ff43cdffb15da
Aashish <60545932+aashishjha99@users.noreply.github.com>
2022-01-16 05:23:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 
 import React, { Component } from "react";
 import Nav from "./Component/Nav";
-import newsitem from "./Component/newsitem";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <news />
       </div>
     );
   }
