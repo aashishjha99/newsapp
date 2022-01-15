28236cc01fc5591029b54ac233c2f770626e2666
Aashish <60545932+aashishjha99@users.noreply.github.com>
2022-01-16 05:01:50 +0530

    initial

---
+++ src/Component/news.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class news extends Component {
+  render() {
+    return (
+      <div>
+        this is a news component
+        <newsitem />
+      </div>
+    );
+  }
+}
+
+export default news;
---
+++ src/Component/newsitem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class newsitem extends Component {
+  render() {
+    return <div>this is news item</div>;
+  }
+}
+
+export default newsitem;
